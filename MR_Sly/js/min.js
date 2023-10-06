4053de5381413eaaaba36cb96e3548ba86b97de1
carlosjnr0 <105026245+carlosjnr0@users.noreply.github.com>
2023-10-06 15:30:51 +0000

    lastest

--- MR_Sly/js/min.js
+++ MR_Sly/js/min.js
@@ -5,29 +5,42 @@ let c3 = document.getElementById('card3');
 let c4 = document.getElementById('card4');
 let c5 = document.getElementById('card5');
 let c6 = document.getElementById('card6');
+let hh1 = document.getElementById('hcn1');
+let hh2 = document.getElementById('hcn2');
+let hh3 = document.getElementById('hcn3');
+let hh4 = document.getElementById('hcn4');
+let hh5 = document.getElementById('hcn5');
+let hh6 = document.getElementById('hcn6');
+let conheader = document.getElementById('cnhid')
 
 function cd1(){
     content.classList.toggle('factive')
+    conheader.textContent = hh1.textContent+' Airtel Tigo'
     content.focus()
 }
 function cd2(){
     content.classList.toggle('factive')
+    conheader.textContent = hh2.textContent+' Airtel Tigo'
     content.focus()
 }
 function cd3(){
     content.classList.toggle('factive')
+    conheader.textContent = hh3.textContent+' Airtel Tigo'
     content.focus()
 }
 function cd4(){
     content.classList.toggle('factive')
+    conheader.textContent = hh4.textContent+' Airtel Tigo'
     content.focus()
 }
 function cd5(){
     content.classList.toggle('factive')
+    conheader.textContent = hh5.textContent+' Airtel Tigo'
     content.focus()
 }
 function cd6(){
     content.classList.toggle('factive')
+    conheader.textContent = hh6.textContent+' Airtel Tigo'
     content.focus()
 }
 function cls(){
