75f2c14a10c1b08b8d9f2f6a49ae5ab042a43c8b
carlosjnr0 <105026245+carlosjnr0@users.noreply.github.com>
2023-10-07 06:07:42 +0000

    update

--- MR_Sly/js/min.js
+++ MR_Sly/js/min.js
@@ -5,12 +5,32 @@ let c3 = document.getElementById('card3');
 let c4 = document.getElementById('card4');
 let c5 = document.getElementById('card5');
 let c6 = document.getElementById('card6');
+let c7 = document.getElementById('card7');
+let c8 = document.getElementById('card8');
+let c9 = document.getElementById('card9');
+let c10 = document.getElementById('card10');
+let c11 = document.getElementById('card11');
+let c12 = document.getElementById('card12');
+let c13 = document.getElementById('card13');
+let c14 = document.getElementById('card14');
+let c15 = document.getElementById('card15');
+let c16 = document.getElementById('card16');
 let hh1 = document.getElementById('hcn1');
 let hh2 = document.getElementById('hcn2');
 let hh3 = document.getElementById('hcn3');
 let hh4 = document.getElementById('hcn4');
 let hh5 = document.getElementById('hcn5');
 let hh6 = document.getElementById('hcn6');
+let hh7 = document.getElementById('hcn7');
+let hh8 = document.getElementById('hcn8');
+let hh9 = document.getElementById('hcn9');
+let hh10 = document.getElementById('hcn10');
+let hh11 = document.getElementById('hcn11');
+let hh12 = document.getElementById('hcn12');
+let hh13 = document.getElementById('hcn13');
+let hh14 = document.getElementById('hcn14');
+let hh15 = document.getElementById('hcn15');
+let hh16 = document.getElementById('hcn16');
 let conheader = document.getElementById('cnhid')
 let hdd1 = document.getElementById('hd1')
 let hdd2 = document.getElementById('hd2')
@@ -50,6 +70,56 @@ function cd5(){
 function cd6(){
     content.classList.toggle('factive')
     conheader.textContent = hh6.textContent+' Airtel Tigo'
+    content.focus()
+}
+function cd7(){
+    content.classList.toggle('factive')
+    conheader.textContent = hh7.textContent+' Airtel Tigo'
+    content.focus()
+}
+function cd8(){
+    content.classList.toggle('factive')
+    conheader.textContent = hh8.textContent+' Airtel Tigo'
+    content.focus()
+}
+function cd9(){
+    content.classList.toggle('factive')
+    conheader.textContent = hh9.textContent+' Airtel Tigo'
+    content.focus()
+}
+function cd10(){
+    content.classList.toggle('factive')
+    conheader.textContent = hh10.textContent+' Airtel Tigo'
+    content.focus()
+}
+function cd11(){
+    content.classList.toggle('factive')
+    conheader.textContent = hh11.textContent+' Airtel Tigo'
+    content.focus()
+}
+function cd12(){
+    content.classList.toggle('factive')
+    conheader.textContent = hh12.textContent+' Airtel Tigo'
+    content.focus()
+}
+function cd13(){
+    content.classList.toggle('factive')
+    conheader.textContent = hh13.textContent+' Airtel Tigo'
+    content.focus()
+}
+function cd14(){
+    content.classList.toggle('factive')
+    conheader.textContent = hh14.textContent+' Airtel Tigo'
+    content.focus()
+}
+function cd15(){
+    content.classList.toggle('factive')
+    conheader.textContent = hh15.textContent+' Airtel Tigo'
+    content.focus()
+}
+function cd16(){
+    content.classList.toggle('factive')
+    conheader.textContent = hh16.textContent+' Airtel Tigo'
     content.focus()
 }
 function cls(){
